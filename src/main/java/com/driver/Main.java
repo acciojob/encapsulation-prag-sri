deb4337747f140c8ae50334760708f55614faad4
Pragati <pragati.sri2411@gmail.com>
2022-12-19 16:04:17 +0530

    Problem Solved 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args)
+  {
+      RWOnly obj= new RWOnly();
+      obj.setName("Pragati");
+      System.out.println(obj.getName());
+      //'name' has private access in 'com.driver.RWOnly'
+  }
 }
